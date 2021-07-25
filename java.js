1c01e0146eba95176243c52b2a553077c77e98e8
SwixGaming <82118300+SwixGaming@users.noreply.github.com>
2021-07-25 17:46:56 -0400

    Create java.js

---
+++ java.js
@@ -0,0 +1,7 @@
+<button id="myButton" class="float-left submit-button" >Home</button>
+
+<script type="text/javascript">
+    document.getElementById("myButton").onclick = function () {
+        location.href = "https://swixgaming.github.io/3d-model-editing/board.html";
+    };
+</script>
